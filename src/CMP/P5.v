d61702f632a95cfda2544d1a58aa7d718466c793
Gustav Behm <gustav.behm@gmail.com>
2018-02-08 20:25:45 +0100

    Prove option_streamless lemma

--- src/CMP/P5.v
+++ src/CMP/P5.v
@@ -18,10 +18,42 @@ Proof.
   intros n. case (f n); intro; reflexivity.
 Qed.
 
+Lemma non_empty_option_streamless {X: Set} {f: nat -> option X} {x0}:
+  f 0 = Some x0 -> streamless X ->
+  {i : nat & {j : nat & i <> j /\ f i = f j}}.
+Proof.
+  intros H sx.
+  destruct (sx (fun n => match f (S n) with None => x0 | Some x => x end))
+    as [i [j [p0 p1]]].
+  case_eq (f (S i)); case_eq (f (S j)).
+  - intros xj Fj xi Fi.
+    exists (S i), (S j).
+    split; auto. rewrite Fi, Fj in *. now rewrite p1.
+  - intros Fj xi Fi.
+    exists (S i), 0.
+    split; auto. rewrite Fi, Fj in *. now rewrite H, p1.
+  - intros xj Fj Fi.
+    exists 0, (S j).
+    split; auto. rewrite Fi, Fj in *. now rewrite H, p1.
+  - intros Fj Fi.
+    exists (S i), (S j).
+    split; auto. now rewrite Fj, Fi.
+Qed.
+
 Lemma option_streamless {X}: streamless X -> streamless (option X).
 Proof.
-  intros s fs.
-Admitted.
+  intros sx f.
+  case_eq (f 0).
+  - intros x0 H0.
+    apply (non_empty_option_streamless H0 sx).
+  - case_eq (f 1).
+    + intros x0 H0 _.
+      pose (f' := fun n => f (S n)).
+      replace (f 1) with (f' 0) in H0 by auto.
+      destruct (non_empty_option_streamless H0 sx) as [i [j [p0 p1]]].
+      exists (S i), (S j); auto.
+    + intros H0 H1. exists 0, 1. split; [auto|now rewrite H0, H1].
+Qed.
 
 Lemma product_streamless {X Y}: streamless X -> streamless Y -> streamless (X*Y).
 Proof.
